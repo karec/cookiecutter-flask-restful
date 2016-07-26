13ab36e09d829f33a1a923e2b9a934c609ad8cf4
karec <manu.valette@gmail.com>
2016-07-26 23:19:35 +0200

    add basic configuration and update extensions

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/config.py
@@ -0,0 +1,36 @@
+import os
+
+DEBUG = False
+TESTING = False
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///api.db'
+
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'root': {
+        'level': 'NOTSET',
+        'handlers': ['default'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s: %(levelname)s / %(name)s] %(message)s',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        '{{cookiecutter.app_name}}': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': False,
+        }
+    }
+}
--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
@@ -1,2 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+
+db = SQLAlchemy()
+marshmallow = Marshmallow()
