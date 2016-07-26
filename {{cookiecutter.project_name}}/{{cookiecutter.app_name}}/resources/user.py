2d20d564fc4f5dd50eccd443b98424e3223385b7
karec <manu.valette@gmail.com>
2016-07-26 23:59:45 +0200

    update resources

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/resources/user.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/resources/user.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import request
 from flask_restful import Resource
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -20,28 +20,39 @@ class UserDetail(Resource):
     def get(self, user_id):
         try:
             user = db.session.query(User).filter_by(id=user_id).one()
-            return jsonify(self.schema.dump(user))
+            data, errors = self.schema.dump(user)
+
+            if errors:
+                return errors, 422
+
+            return data
         except NoResultFound:
-            return jsonify({"message": "User not found"}), 404
-
-    def post(self):
-        data, errors = self.schema.load(reuest.get_json())
-        if errors:
-            return jsonify(errors), 422
-
-        user = User(**data)
-        db.session.add(user)
-        db.session.commit()
-
-        return jsonify(self.schema.dump(user)), 201
+            return {"message": "User not found"}, 404
 
 
 class UserList(Resource):
 
-    schema = UserSchema(many=True)
-
     def get(self):
+        schema = UserSchema(many=True)
         users = db.session.query(User).all()
-        results = self.schema.dump(users)
+        results, errors = schema.dump(users)
 
-        return jsonify(results)
+        if errors:
+            return errors, 422
+
+        return results
+
+    def post(self):
+        schema = UserSchema()
+
+        try:
+            user, errors = schema.load(request.get_json())
+            if errors:
+                return errors, 422
+
+            db.session.add(user)
+            db.session.commit()
+            return schema.dump(user).data, 201
+        except Exception as e:
+            db.session.rollback()
+            return str(e), 500
