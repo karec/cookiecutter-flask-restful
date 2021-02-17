1a43389d1bdffe218c5f543c5eccb9b89b3c490b
Eduardo <eduardo@origen.ai>
2021-02-17 16:47:43 +0100

    propossed solution to fix the pagination issue

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/commons/pagination.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/commons/pagination.py
@@ -6,20 +6,27 @@ DEFAULT_PAGE_SIZE = 50
 DEFAULT_PAGE_NUMBER = 1
 
 
+def extract_pagination(page=None, per_page=None, **request_args):
+    page = int(page) if page is not None else DEFAULT_PAGE_NUMBER
+    per_page = int(per_page) if per_page is not None else DEFAULT_PAGE_SIZE
+    return page, per_page, request_args
+
+
 def paginate(query, schema):
-    page = request.args.get("page", DEFAULT_PAGE_NUMBER)
-    per_page = request.args.get("page_size", DEFAULT_PAGE_SIZE)
+    page, per_page, other_request_args = extract_pagination(**request.args)
     page_obj = query.paginate(page=page, per_page=per_page)
     next_ = url_for(
         request.endpoint,
         page=page_obj.next_num if page_obj.has_next else page_obj.page,
         per_page=per_page,
+        **other_request_args,
         **request.view_args
     )
     prev = url_for(
         request.endpoint,
         page=page_obj.prev_num if page_obj.has_prev else page_obj.page,
         per_page=per_page,
+        **other_request_args,
         **request.view_args
     )
 
